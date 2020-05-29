da8098eab6d76e6974e97d4c1a1f7fdb41d99b2e
dreamer2q <dreamer22qq@gmail.com>
2020-05-29 22:21:42 +0800

    update

---
+++ Learn/homework/395/9-1.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright: MIT
+ * ----
+ * File: 9-1.cpp
+ * File Created: Thursday, 28th May 2020 9:25:07 am
+ * Author: dreamer2q (dreamer22qq@gmail.com)
+ * Last Modified: Thursday, 28th May 2020 9:25:29 am
+ * Modified By: dreamer2q (dreamer22qq@gmail.com>)
+ * -----
+ * Description: 
+ * 
+ * 作业 9-1
+ */
+
+
+#include <bits/stdc++.h>
+#include <array>
+
+using namespace std;
+
+int main(){
+
+
+  
+
+    return 0;
+}
